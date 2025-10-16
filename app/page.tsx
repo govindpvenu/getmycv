5454458bf17999b9d8e655391c5948ee558c114e
govindpvenu <govindpvenu.txt@gmail.com>
2025-10-16 09:02:37 +0530

    feat:home

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
-import { LoginForm } from "@/components/LoginForm";
 import ThemeToggle from "@/components/ThemeToggle";
+import { HyperText } from "@/components/ui/hyper-text";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
+      <HyperText className="text-primary font- from-accent to-primary bg-clip-text text-4xl font-extrabold tracking-tight  animate-gradient-x">
+        GetMyCV
+      </HyperText>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/sign-up">Sign Up</Link>
+      <Link href="/dashboard">Dashboard</Link>
       <ThemeToggle />
-      <LoginForm />
     </div>
   );
 }
