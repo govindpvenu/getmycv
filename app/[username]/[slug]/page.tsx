a5a42c4bebb205c6e0a56ed7db66eb2de9c38137
govindpvenu <govindpvenu.txt@gmail.com>
2025-11-06 13:39:37 +0530

    feat: resume preview done

--- app/[username]/[slug]/page.tsx
+++ app/[username]/[slug]/page.tsx
@@ -1,6 +1,10 @@
+import ThemeToggle from "@/components/ThemeToggle";
+import { Button } from "@/components/ui/button";
 import { db } from "@/db/drizzle";
 import { container, user } from "@/db/schemas";
 import { and, eq } from "drizzle-orm";
+import { Download } from "lucide-react";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 export default async function ResumePage({
@@ -36,10 +40,32 @@ export default async function ResumePage({
     return <div>Resume is private</div>;
   }
   return (
-    <>
-      <div>ResumePage: {containerData?.title}</div>
-      <div>Resume url: {containerData?.resumeUrl}</div>
-      <div>{JSON.stringify(containerData)}</div>
-    </>
+    <main className="flex h-dvh w-full justify-center items-center  rounded  sm:mx-auto sm:my-16 ">
+      <iframe
+        src={`${containerData.resumeUrl}#toolbar=0&navpanes=0&scrollbar=1&view=FitH`}
+        title="Resume"
+        className="h-full w-full max-w-2xl border rounded-md"
+      />
+      <div className="fixed bottom-5 right-5 z-0 hidden sm:block print:hidden">
+        <div className="flex flex-col items-center gap-y-2">
+          <a
+            href={`${containerData.resumeUrl}?download=1`}
+            target="_blank"
+            download={`${containerData.title}.pdf`}
+            rel="noopener noreferrer"
+          >
+            <Button
+              size="icon"
+              className="hover:bg-muted size-9  rounded-full"
+              variant="outline"
+            >
+              <Download size={20} />
+            </Button>
+          </a>
+
+          <ThemeToggle />
+        </div>
+      </div>
+    </main>
   );
 }
