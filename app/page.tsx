066bbcb27b795f31ce8d1232ccb1f32c0539d7d8
govindpvenu <govindpvenu.txt@gmail.com>
2025-11-12 20:22:30 +0530

    feat: add footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/tailark/hero-section";
+import FooterSection from "@/components/tailark/footer";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <FooterSection />
     </main>
   );
 }
---
+++ components/tailark/footer.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import GetMyCV from "../GetMyCV";
+
+const links = [
+  {
+    title: "Features",
+    href: "#",
+  },
+  {
+    title: "Solution",
+    href: "#",
+  },
+  {
+    title: "Customers",
+    href: "#",
+  },
+  {
+    title: "Pricing",
+    href: "#",
+  },
+  {
+    title: "Help",
+    href: "#",
+  },
+  {
+    title: "About",
+    href: "#",
+  },
+];
+
+export default function FooterSection() {
+  return (
+    <footer className="py-16 md:py-32">
+      <div className="mx-auto max-w-5xl px-6">
+        <div className="flex justify-center w-full">
+          <GetMyCV />
+        </div>
+        <div className="my-8 flex flex-wrap justify-center gap-6 text-sm">
+          {links.map((link, index) => (
+            <Link
+              key={index}
+              href={link.href}
+              className="text-muted-foreground hover:text-primary block duration-150"
+            >
+              <span>{link.title}</span>
+            </Link>
+          ))}
+        </div>
+
+        <span className="text-muted-foreground block text-center text-sm">
+          {" "}
+          © {new Date().getFullYear()} Tailark, All rights reserved
+        </span>
+      </div>
+    </footer>
+  );
+}
