a607b424f19f71e37294a331fdd68dc3d772a310
govindpvenu <govindpvenu.txt@gmail.com>
2025-11-13 15:31:45 +0530

    feat: stats section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/tailark/hero-section";
 import FooterSection from "@/components/tailark/footer";
 import CallToAction from "@/components/tailark/call-to-action";
+import StatsSection from "@/components/tailark/stats";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <StatsSection />
       <CallToAction />
       <FooterSection />
     </main>
